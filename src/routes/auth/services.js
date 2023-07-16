109ba5f865d1942afc95376429e4906c28795fe7
Zeeshan <zeeshansheray1@gmail.com>
2023-07-16 17:03:22 +0500

    updates

--- src/routes/auth/services.js
+++ src/routes/auth/services.js
@@ -2,11 +2,12 @@
 
 const GetDataService = async () => {
     try {
-      const query = 'SELECT * FROM des54556';
+      const query = 'SELECT * FROM des54556 LIMIT 50';
   
       const results = await global.pool.query(query);
       const data = results.rows;
   
+      console.log('data ', data.length)
       return {
         success: data.length > 0,
         message: data.length > 0 ? 'Users found successfully' : 'Cannot find users',
