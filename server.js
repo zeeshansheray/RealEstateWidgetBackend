472ec4c7f6743aa222da551708395c08d97a84f2
zeeshan <zeeshansheray1@gmail.com>
2023-07-17 12:47:57 +0500

    changes

--- server.js
+++ server.js
@@ -1,45 +1,53 @@
-const express = require('express');
-const compression = require('compression');
-const cors = require('cors');
-const path = require('path');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require('express')
+const compression = require('compression')
+const cors = require('cors')
+const path = require('path')
 
-const env = require('./src/_config/config');
-const connectDb = require('./src/database/db');
-const endpoints = require('./src/routes');
+const env = require('./src/_config/config')
+const connectDb = require('./src/database/db')
+const endpoints = require('./src/routes')
 const bodyParser = require('body-parser');
-const fs = require('fs');
 
-const app = express();
+const app = express()
 app.use(cors());
 
 // Connect to Database
-connectDb();
+connectDb()
+
+// app.use(express.urlencoded({extended: false, limit: '50mb'}))
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json())
+
+app.use(compression())
+app.use(express.static(path.join(__dirname, 'build')))
+app.set('view engine', 'ejs')
 
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json());
+// app.options('*', cors(env.corsOption))
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin,Content-Type,x-auth-user,x-amz-meta-fieldname,x-auth-token");
+//     next();
+// });
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, 'build')));
-app.set('view engine', 'ejs');
 
-const server = http.createServer(app);
+app.listen(env.port, () => {
+    console.log('Server is listening at', env.port, 'with env', process.env.NODE_ENV);
+})
 
-server.listen(env.port, () => {
-  console.log('Server is listening at', env.port, 'with env', process.env.NODE_ENV);
+endpoints(app)
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandeled Rejection\n ", err);
 });
-
-
-
-endpoints(app);
-
-
-process.on('unhandledRejection', (err) => {
-});
-
-process.on('uncaughtException', (err) => {
-  // console.log('Uncaught Rejection\n ', err);
+  
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught Rejection\n ", err);
 });
