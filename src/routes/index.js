0aee77ac226383542903ce5f328f4131ca4098d3
zeeshan <zeeshansheray1@gmail.com>
2023-07-17 12:32:44 +0500

    changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ const middleware = process.env.NODE_ENV === 'production' ? '/v1/' : '/api/v1/'
 const Auth      = require("./auth/routes")
 
 
+console.log('process.env.NODE_ENV  ', process.env.NODE_ENV )
+
 const initializeEndpoints = (app) => {
     app.use(middleware + 'auth', Auth)
 };
